82d51949e6edb75d4a0232a816f498d7da37414b
Serhat Şahin <ssahin@atlassian.com>
2020-01-19 21:57:40 +0300

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by ssahin on 19.01.2020.
+//  Copyright © 2020 ssahin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
